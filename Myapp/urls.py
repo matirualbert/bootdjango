e19176236a0bce287f410d0afdd0fd41b91cc456
eMobilis <albertnjoroge50@gmail.com>
2024-02-23 11:02:10 +0300

    initial commit

---
+++ Myapp/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from Myapp import views
+
+urlpatterns = [
+    path('', views.home, name='my_index'),
+    path('', views.about, name='my_about'),
+    path('', views.contact, name='my_contact'),
+    path('', views.call, name='my_call'),
+    path('', views.home, name='my_index')
+    path('', views.home, name='my_index')
+    path('', views.home, name='my_index')
+    path('', views.home, name='my_index')
+    path('', views.home, name='my_index')
+]
